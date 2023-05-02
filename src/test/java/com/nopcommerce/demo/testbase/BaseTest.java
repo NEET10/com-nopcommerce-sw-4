189c27b2a9e2fefa70ed73377617b6aa57578710
Nita0310 <neetgabani@gmail.com>
2023-05-02 22:49:13 +0100

    com-nopcommerce-sw-4

---
+++ src/test/java/com/nopcommerce/demo/testbase/BaseTest.java
@@ -0,0 +1,21 @@
+package com.nopcommerce.demo.testbase;
+
+import com.nopcommerce.demo.propertyreader.PropertyReader;
+import com.nopcommerce.demo.utilities.Utility;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest extends Utility {
+    String browser = PropertyReader.getInstance().getProperty("browser");
+    @BeforeTest
+
+    public void setUp(){
+        selectBrowser(browser);
+
+    }
+    @AfterTest
+    public void tearDown(){
+        closeBrowser();
+
+    }
+}
